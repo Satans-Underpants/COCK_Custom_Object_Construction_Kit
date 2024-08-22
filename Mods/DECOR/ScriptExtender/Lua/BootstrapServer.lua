66c13100a7d6dc3a92089886001e09f55cfadbc2
Satans-Underpants <128394890+Satans-Underpants@users.noreply.github.com>
2024-08-22 02:07:55 +0200

    progress

--- Mods/DECOR/ScriptExtender/Lua/BootstrapServer.lua
+++ Mods/DECOR/ScriptExtender/Lua/BootstrapServer.lua
@@ -3,3 +3,6 @@ Ext.Require("Server/ObjectReading.lua")
 Ext.Require("Server/Library.lua")
 Ext.Require("Server/SpellAddition.lua")
 Ext.Require("Server/Utilities.lua")
+
+
+Ext.Osiris.RegisterListener("Equipped", 2, "after", function(item, character)  end)
